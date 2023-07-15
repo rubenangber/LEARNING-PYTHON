525149c2d49cd2bdcb558b8ad669ea15e1b2bd0c
rubenangber <rubigrowl@gmail.com>
2023-07-15 15:59:47 +0200

    Ejercicios🔆

---
+++ OpenBootcamp/Ejercicio 8/main.py
@@ -0,0 +1,11 @@
+import calculadora as calc
+
+sum = calc.suma(3, 5)
+res = calc.resta(5, 1)
+mul = calc.multiplicacion(10, 9)
+div = calc.division(10, 5)
+
+print(sum)
+print(res)
+print(mul)
+print(div)
