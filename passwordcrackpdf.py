bfe702fff3619f917cc5a29f278646fed7adb5bd
rubenangber <rubigrowl@gmail.com>
2023-10-03 10:31:00 +0200

    Password crack PDF

--- passwordcrackpdf.py
+++ passwordcrackpdf.py
@@ -1,9 +1,10 @@
 import PyPDF2
 
-pdf_pass = PyPDF2.PdfFileReader(open('encrypted.pdf'))
-with open('passwords.txt', 'r', encoding='utf-8') as f:
+encryptedpdf = input('Enter the name of the encrypted pdf file: ')
+pdf_pass = PyPDF2.PdfFileReader(open(encryptedpdf))
+with open('passwords.txt', 'r', encoding = 'utf-8') as f:
     for line in f:
         password = line.strip()
         if pdf_pass.decrypt(password) != 0:
             print(f'Password found: {password}')
-            break 
+            break
